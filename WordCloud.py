ba771bbb4f72fc40fa1a91169d2b037b13907393
Yally-661 <yukiyada922@gmail.com>
2020-10-15 13:12:07 +0900

    Update WordCloud.py

--- WordCloud.py
+++ WordCloud.py
@@ -1,32 +1,62 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[26]:
+# In[1]:
 
 
 from PIL import Image
 import numpy as np
+import MeCab
 from wordcloud import WordCloud
+import GetTweet as gt
 
 
-# In[42]:
+# In[35]:
 
 
-mask = np.array(Image.open('apple.png'))
+m = MeCab.Tagger ()
 
 
 # In[43]:
 
 
+text = gt.get_tweet_text('Cuscha_661')
+
+
+# In[44]:
+
+
+splitted = []
+
+
+# In[45]:
+
+
+for x in m.parse(text).splitlines()[:-1] :
+   if x.split("\t")[1].split(",")[0] in ["名詞"]:
+       splitted.append(x.split("\t")[0])
+
+
+# In[46]:
+
+
+splitted
+
+
+# In[47]:
+
+
+mask = np.array(Image.open('apple.png'))
+
+
+# In[48]:
+
+
 mask = np.where(mask == 0, 0, 255)
 
 
-# In[59]:
+# In[49]:
 
-
-# 作成したテキストの読み込み
-with open('analyze2.txt', 'r') as f:
-    text = f.read()
 
 # 除外したい単語
 stop_text = []
@@ -34,53 +64,20 @@ stop_text = []
 # wordcloudの設定
 wordcloud = WordCloud(mask = mask,
             background_color = 'white',
-            font_path = 'ProductSans-Regular.ttf',
+            font_path = 'C:\Windows\Fonts\游ゴシック\YuGothM.ttc',
             collocations = False,
             stopwords = stop_text,
-            contour_width=1,contour_color='steelblue').generate(text)
+            contour_width=1,contour_color='steelblue').generate(' '.join(splitted))
 
 # wordcloudの作成
 wordcloud.to_file('./wordcloud/wordcloud.png')
 
 
-# In[49]:
-
-
-import MeCab
-
-
-# In[62]:
-
-
-m = MeCab.Tagger ()
-print(m.parse ("すもももももももものうち"))
-
-
-# In[86]:
-
-
-splitted = ' '.join([x.split("\t")[0] for x in m.parse(text).splitlines()[:-1] if x.split("\t")[1].split(",")[0] not in ["助詞", "助動詞"]])
-
-
-# In[87]:
-
-
-splitted
-
-
-# In[85]:
-
-
-for i in m.parse(text).splitlines():
-    splitted += i.split('\t')[0]
-    print(splitted)
-
-
-# In[1]:
+# In[58]:
 
 
 import subprocess
-subprocess.run(['jupyter', 'nbconvert', '--to', 'python', 'textMining.ipynb'])
+subprocess.run(['jupyter', 'nbconvert', '--to', 'python', 'GetTweet.ipynb'])
 
 
 # In[ ]:
