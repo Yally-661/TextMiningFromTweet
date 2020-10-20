5c97a9062d670d36adf90d97cc7a393f3874bd39
Yally-661 <yukiyada922@gmail.com>
2020-10-20 22:46:55 +0900

    Update GetTweet.py

--- GetTweet.py
+++ GetTweet.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[4]:
+# In[7]:
 
 
 import re
@@ -10,7 +10,7 @@ import sys
 from requests_oauthlib import OAuth1Session
 
 
-# In[5]:
+# In[8]:
 
 
 CONSUMER_KEY = 'GH9vERo5nlddVVREXFIpaGAst'
@@ -19,33 +19,99 @@ ACCESS_TOKEN = '1090500433-zanofeFnpZf0PeJrkhCuk6FzF1Cgq1mq7etWwzN'
 ACCESS_TOKEN_SECRET = '7JA2j5D6sCc9o9VX2S6IZgp17QTqxNj9cNHkQEVc94yAX'
 
 
-# In[7]:
+# In[9]:
 
 
-#ツイートを抽出
-def __get_tweet(user_id):
-    twitter = OAuth1Session(CONSUMER_KEY,CONSUMER_KEY_SECRET,ACCESS_TOKEN,ACCESS_TOKEN_SECRET)
-    url = 'https://api.twitter.com/1.1/statuses/user_timeline.json'
-    params = {'count':'1000','screen_name':user_id}
-    
-    res = twitter.get(url,params = params)
-    timelines = json.loads(res.text)
-    tweets = ''
-    for x in timelines:
-        tweets += x['text'] + ' '
-    
-    return tweets
+class TwitterAPI:
+    def __fetch_timelines(self,user_id):
+        '''
+        Twitter APIから指定したユーザーの過去ツイートを取得する(RT除く)
+
+        Parameters
+        ----------
+        user_id : str
+            ツイートを取得するユーザーID
+
+        Returns
+        -------
+        timelines : dict
+            ツイートデータ
+            200件分
+
+        '''
+        twitter = OAuth1Session(CONSUMER_KEY,CONSUMER_KEY_SECRET,ACCESS_TOKEN,ACCESS_TOKEN_SECRET)
+        url = 'https://api.twitter.com/1.1/statuses/user_timeline.json'
+        params = {'count':'200','include_rts':'false','screen_name':user_id}
+        res = twitter.get(url,params = params)
+        timelines = json.loads(res.text)
+        return timelines 
+            
+
+    def __create_text_from_timelines(self,timelines):
+        '''
+        Twitter APIから抽出したタイムラインの本文を文字列にまとめる
+
+        Parameters
+        ----------
+        timelines : dict
+            Twitter APIから取得したタイムライン
+
+        Returns
+        -------
+        tweet_text : str
+            ツイートの本文をすべてまとめたもの(半角スペース区切り)
+
+        '''
+        tweet_text = ''
+        for tweet in timelines:
+            tweet_text += tweet['text'] + ' '
+        return tweet_text
 
 
-# In[8]:
+    def get_timeline(self,user_id):
+        '''
+        ユーザーIDからTwitterタイムラインの本文テキストを取得する(RT除く)
+
+        Parameters
+        ----------
+        user_id : str
+            タイムラインを取得するユーザーID
+
+        Returns
+        -------
+        tweet_text : str
+            ツイートの本文をすべてまとめたもの(半角スペース区切り)
+
+        '''    
+        timelines = self.__fetch_timelines(user_id)
+        tweet_text = self.__create_text_from_timelines(timelines)
+        return tweet_text
 
 
-#getter
-def get_tweet_text(tweet_user):
-    return  __get_tweet(tweet_user)
+# In[10]:
 
 
-# In[12]:
+def get_timeline(user_id):
+    '''
+    ユーザーIDからTwitterタイムラインの本文テキストを取得する(RT除く)
+
+    Parameters
+    ----------
+    user_id : str
+        タイムラインを取得するユーザーID
+
+    Returns
+    -------
+    tweet_text : str
+        ツイートの本文をすべてまとめたもの(半角スペース区切り)
+
+    '''               
+    api = TwitterAPI()
+    tweet_text = api.get_timeline(user_id)
+    return tweet_text
+
+
+# In[14]:
 
 
 import subprocess
