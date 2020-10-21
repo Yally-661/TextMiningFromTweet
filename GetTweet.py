441fb9edd4e57afde22d1a0ad19780f6cfc1c60d
Yally-661 <yukiyada922@gmail.com>
2020-10-21 22:47:14 +0900

    Update GetTweet.py

--- GetTweet.py
+++ GetTweet.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[7]:
+# In[3]:
 
 
 import re
@@ -10,22 +10,27 @@ import sys
 from requests_oauthlib import OAuth1Session
 
 
-# In[8]:
+# In[4]:
 
 
-CONSUMER_KEY = 'GH9vERo5nlddVVREXFIpaGAst'
-CONSUMER_KEY_SECRET = 'K6Rs5QeT7v9i0b5d5SlxxN4rJqaE9O4dxV5bgdFnKnvpCsMbUI'
-ACCESS_TOKEN = '1090500433-zanofeFnpZf0PeJrkhCuk6FzF1Cgq1mq7etWwzN'
-ACCESS_TOKEN_SECRET = '7JA2j5D6sCc9o9VX2S6IZgp17QTqxNj9cNHkQEVc94yAX'
+CK = 'GH9vERo5nlddVVREXFIpaGAst' #CONSUMER_KEY
+CS = 'K6Rs5QeT7v9i0b5d5SlxxN4rJqaE9O4dxV5bgdFnKnvpCsMbUI' #CONSUMER_KEY_SECRET
+AT = '1090500433-zanofeFnpZf0PeJrkhCuk6FzF1Cgq1mq7etWwzN' #ACCESS_TOKEN
+AS = '7JA2j5D6sCc9o9VX2S6IZgp17QTqxNj9cNHkQEVc94yAX' #ACCESS_TOKEN_SECRET
 
 
-# In[9]:
+# In[5]:
 
 
 class TwitterAPI:
+    
+    def __init__(self):
+        self.session = OAuth1Session(CK, CS, AT, AS)
+        self.max_id = 0
+        
     def __fetch_timelines(self,user_id):
         '''
-        Twitter APIから指定したユーザーの過去ツイートを取得する(RT除く)
+        Twitter APIから指定したユーザーの過去ツイートをすべて取得する(RT除く)
 
         Parameters
         ----------
@@ -39,11 +44,20 @@ class TwitterAPI:
             200件分
 
         '''
-        twitter = OAuth1Session(CONSUMER_KEY,CONSUMER_KEY_SECRET,ACCESS_TOKEN,ACCESS_TOKEN_SECRET)
+        tweet_list = []
+        cnt = 1
         url = 'https://api.twitter.com/1.1/statuses/user_timeline.json'
         params = {'count':'200','include_rts':'false','screen_name':user_id}
-        res = twitter.get(url,params = params)
-        timelines = json.loads(res.text)
+        while True:
+            if self.max_id != 0:
+                params['max_id'] = self.max_id
+            res = self.session.get(url,params = params)
+            timelines = json.loads(res.text)
+            if len(timelines) == 0:
+                break
+            tweet_list = self.__picup_tweet_text(timelines)
+            print(str(cnt)+'ループ目'+' max_id:' + str(self.max_id))
+            cnt += 1
         return timelines 
             
 
@@ -67,7 +81,16 @@ class TwitterAPI:
             tweet_text += tweet['text'] + ' '
         return tweet_text
 
-
+    
+    def __picup_tweet_text(self,timelines):
+        tweet_list = []
+        for tweet in timelines:
+            tweet_list.append(tweet['text'])
+        self.max_id = tweet['id']-1
+        
+        return tweet_list
+        
+        
     def get_timeline(self,user_id):
         '''
         ユーザーIDからTwitterタイムラインの本文テキストを取得する(RT除く)
@@ -84,11 +107,11 @@ class TwitterAPI:
 
         '''    
         timelines = self.__fetch_timelines(user_id)
-        tweet_text = self.__create_text_from_timelines(timelines)
+       # tweet_text = self.__create_text_from_timelines(timelines)
         return tweet_text
 
 
-# In[10]:
+# In[6]:
 
 
 def get_timeline(user_id):
