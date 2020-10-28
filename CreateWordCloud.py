c2b863072b783d0f4679c54e554a240e01f9d789
Yally-661 <yukiyada922@gmail.com>
2020-10-28 21:59:18 +0900

    Update CreateWordCloud.py
    最終整形

--- CreateWordCloud.py
+++ CreateWordCloud.py
@@ -1,55 +1,79 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
-
 
 from PIL import Image
 import numpy as np
+from wordcloud import STOPWORDS
 from wordcloud import WordCloud
-import GetTweet as gt
+from GetTweet import TwitterAPI
 import MorphologicalAnalysis as ma
+from matplotlib import pyplot as plt
 
 
-# In[2]:
+def create_word_cloud(text,mask = None):
+    '''
+    テキストからワードクラウドを作成し出力する
 
+    Parameters
+    ----------
+    text : str
+        ワードクラウドを作成する文字列
+    mask : ndarray
+        マスクする画像
 
-def create_word_cloud(text,mask):
-    stop_text = ''
-    # wordcloudの設定
+    '''
     wordcloud = WordCloud(mask = mask,
                 background_color = 'white',
                 font_path = 'C:\Windows\Fonts\游ゴシック\YuGothM.ttc',
                 collocations = False,
-                stopwords = stop_text,
+                
                 contour_width = 2,
                 contour_color='black').generate(text)
 
-    # wordcloudの作成
-    wordcloud.to_file('./wordcloud/wordcloud.png')
-    print('ワードクラウド出力')
+    plt.imshow(wordcloud)
+    plt.axis('off')
+    plt.show()
+
+     
+def add_stop_word(stop_list):
+    '''
+    ワードクラウドから除外する文言をセットする
+
+    Parameters
+    ----------
+    stop_list : list
+        ワードクラウドから除外する文言
+    '''
+    for x in stop_list:
+        STOPWORDS.add(x)
 
 
-# In[3]:
+def get_mask(file):
+    '''
+    画像データをワードクラウドのマスク用に加工する
+
+    Parameters
+    ----------
+    file : str
+        マスク用ファイルのパス
+    
+    Return
+    ----------
+    mask : ndarray
+        マスク用加工された画像ファイル
+    '''
+    mask = np.array(Image.open(file))
+    mask = np.where(mask == 0, 0, 255)
+    return mask
 
 
 if __name__ == '__main__':
-    mask = np.array(Image.open('apple.png'))
-    mask = np.where(mask == 0, 0, 255)
-    timeline = gt.get_timeline('Cuscha_661')
+    mask = get_mask(setting.IMAGE)
+    add_stop_word(setting.STOP)
+    timeline = TwitterAPI().get_timeline(setting.USER)
     analyzed = ma.picup_noun(' '.join(timeline))
-    create_word_cloud(' '.join(analyzed),mask)
-
-
-# In[8]:
-
-
-import subprocess
-subprocess.run(['jupyter', 'nbconvert', '--to', 'python', 'CreateWordCloud.ipynb'])
-
-
-# In[ ]:
-
+    create_word_cloud(' '.join(analyzed))
 
 
 
